3417f086c0b6ab4e66f56c267597cae22bae15cd
Odd Karsten <oksvart@gmail.com>
2018-05-08 20:06:01 +0200

    Added json-file that holds API key, is gitignored for now until I find an easy way to encrypt it via git-smudge

--- OddTwitterStreamer/Classes/TweetStreamer.cs
+++ OddTwitterStreamer/Classes/TweetStreamer.cs
@@ -4,14 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace OddTwitterStreamer.Classes
 {
     public class TweetStreamer : ITweetStreamer
     {
+        
+
         public async Task StreamTweets()
         {
-            
+            //Step 1: Get Tweets
+            await Configure();
+
+            //Step 2: Do something with the tweets
+            //"Store"/publish to a repo
+            //Maybe we can read them with text-to-speech?
+        }
+
+        /// <summary>
+        /// Sets up credentials and the API key
+        /// </summary>
+        private async Task Configure()
+        {
+            using (StreamReader jsonFile = File.OpenText(@"key.secure.json"))
+            {
+                var jsonString = await jsonFile.ReadToEndAsync();
+                var definition = new { Key = "", Secret = "" };
+
+                var secretJsonContent = JsonConvert.DeserializeAnonymousType(jsonString, definition);
+
+                var key = secretJsonContent.Key;
+                var secret = secretJsonContent.Secret;
+
+                Auth.SetApplicationOnlyCredentials(key, secret);
+            }
         }
     }
 }

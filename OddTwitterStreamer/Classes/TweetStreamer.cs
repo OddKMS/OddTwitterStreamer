7a38d70b18582b4b4500f0100d0e980371807a14
Odd Karsten <oksvart@gmail.com>
2018-05-11 12:24:30 +0200

    Now properly streams from Twitter. Randomly selects 1% of all english tweets and prints to console.

--- OddTwitterStreamer/Classes/TweetStreamer.cs
+++ OddTwitterStreamer/Classes/TweetStreamer.cs
@@ -7,39 +7,72 @@ using System.Threading.Tasks;
 using Tweetinvi;
 using Newtonsoft.Json;
 using System.IO;
+using Tweetinvi.Models;
+using System.Reactive.Subjects;
 
 namespace OddTwitterStreamer.Classes
 {
     public class TweetStreamer : ITweetStreamer
     {
-        
-
         public async Task StreamTweets()
         {
-            //Step 1: Get Tweets
-            await Configure();
+            //Step 1: 
+            //Get Credentials
+            var credentials = await GetCredentialsFromFile();
+
+            //Step 2: 
+            //Set Credentials for the thread (and child-threads?)
+            Auth.SetCredentials(credentials);
+
+            //NOTE: Mind the rate limit (180 searches pr 15 minutes)
+            //Thus we use Tweetinvi's automatic rate limiter
+            //Also preferable because it's easy and we're using async methods anyway
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+
+            //For the purpose of this program we will not be getting authentication from any user
+            //in order to retrieve tweets, but will use my test account's user credentials
+
+            //Gets the user form current thread credentials
+            var authenticatedUser = User.GetAuthenticatedUser();
+
+            //Step 3:
+            //Creates a stream that outputs 1% of all tweets worldwide (made in english) to the console.
+            var stream = Tweetinvi.Stream.CreateSampleStream(credentials);
+            stream.AddTweetLanguageFilter(LanguageFilter.English);
+
+            stream.TweetReceived += (sender, args) =>
+            {
+                Console.WriteLine(args.Tweet);
+            };
+
+            await stream.StartStreamAsync();
 
             //Step 2: Do something with the tweets
-            //"Store"/publish to a repo
             //Maybe we can read them with text-to-speech?
         }
 
         /// <summary>
-        /// Sets up credentials and the API key
+        /// Gets credentials from file and returns an ITwitterCredentials object
         /// </summary>
-        private async Task Configure()
+        /// <returns>
+        /// ITwitterCredentials object
+        /// </returns>
+        private async Task<ITwitterCredentials> GetCredentialsFromFile()
         {
             using (StreamReader jsonFile = File.OpenText(@"key.secure.json"))
             {
                 var jsonString = await jsonFile.ReadToEndAsync();
-                var definition = new { Key = "", Secret = "" };
+                var definition = new { ApiKey = "", ApiSecret = "",
+                                        AccessToken = "", AccessTokenSecret ="" };
 
-                var secretJsonContent = JsonConvert.DeserializeAnonymousType(jsonString, definition);
+                var jsonContent = JsonConvert.DeserializeAnonymousType(jsonString, definition);
 
-                var key = secretJsonContent.Key;
-                var secret = secretJsonContent.Secret;
+                var key = jsonContent.ApiKey;
+                var secret = jsonContent.ApiSecret;
+                var token = jsonContent.AccessToken;
+                var tokenSecret = jsonContent.AccessTokenSecret;
 
-                Auth.SetApplicationOnlyCredentials(key, secret);
+                return Auth.CreateCredentials(key, secret, token, tokenSecret);
             }
         }
     }
